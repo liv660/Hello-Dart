3a1d6214e03ae6dbfc7152ff336aec55839ba268
sysim <liv660n@gmail.com>
2023-04-25 15:04:03 +0900

    Basic Data Types

---
+++ bin/data_types/basic_data_types.dart
@@ -0,0 +1,11 @@
+void basic_types() {
+  //String, bool, int, double 모두 class 이다.
+  String name = 'soyeon';
+  bool alive = true;
+  int years = 2024;   //extends num
+  double avg = 55.5;  //extends num
+
+  //num은 int, double의 값을 모두 담을 수 있다.
+  num value = 2023;
+  value = 54.5;
+}
