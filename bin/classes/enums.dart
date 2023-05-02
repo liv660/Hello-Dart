8d9b9e75c2216efa243103cece041a73a7af2eb8
sysim <liv660n@gmail.com>
2023-05-02 16:32:21 +0900

    Enums

---
+++ bin/classes/enums.dart
@@ -0,0 +1,20 @@
+enum Team { red, blue, }
+enum XPLevel { beginnger, medium, pro }
+
+class Player {
+  String name;
+  XPLevel xp;
+  Team team;
+
+  Player({required this.name, required this.xp, required this.team});
+
+  void introduce() => print('From now, my team is $team and I\'m $xp');
+}
+
+void main() {
+  var player = Player(name: 'soyeon', xp: XPLevel.beginnger, team: Team.red)
+      ..introduce()
+      ..team = Team.blue
+      ..xp = XPLevel.medium
+      ..introduce();
+}
